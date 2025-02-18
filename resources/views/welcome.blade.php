5a4016dd12ce474730cd3ae52df9f770bf5c9f58
bishoDev123 <bisholiveenglish@gmail.com>
2025-02-18 13:59:20 +0200

    styling changes made for the search bar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,19 @@
 <x-layout>
     <div class="flex justify-center items-center text-white">
         <div class="relative">
-            <img src="{{ asset('images/woman.webp') }}" class="z-10" alt="">
-            <div class="absolute inset-0 flex justify-center items-center text-white">
-                <h1 class="text-4xl font-bold text-center">Find the best jobs in Egypt</h1>
+            <img src="{{ asset('images/woman.webp') }}" class="z-10 brightness-50 w-screen h-screen object-cover" alt="">
+            <div class="absolute inset-0 flex justify-center items-center flex-col text-white mt-auto">
+                <div class="text-left">
+                    <h1 class="text-4xl font-bold mb-3">Find the best jobs in Egypt</h1>
+                    <p class=" text-lg">Searching for vacancies & career opportunities? WUZZUF helps you in your job search in Egypt</p>
+                </div>
+                <input type="text" class="shadow appearance-none border rounded w-96 py-2 px-3 text-gray-700 leading-tight focus:outline-double focus:outline-sky-600 focus:shadow-outline mt-5" placeholder="Search for jobs">
             </div>
+            
         </div>
 
     </div>
+    <script>
+        document.querySelector("input").focus();
+    </script>
 </x-layout>
